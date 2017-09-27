3956f18cb6f03196b1eb35cc6ab63d10f7f75ee1
Minicurso Rails <tiagompalte@hotmail.com>
2017-09-27 11:53:55 -0300

    Primeiro Commit

---
+++ app/controllers/phones_controller.rb
@@ -0,0 +1,21 @@
+class PhonesController < ApplicationController
+  def create
+    @person = Person.find(params[:person_id])
+    @phone	= @person.phones.create(phone_params)
+
+    redirect_to person_path(@person)
+  end
+
+  def destroy
+    @person = Person.find(params[:person_id])
+    @phone  = @person.phones.find(params[:id])
+    @phone.destroy
+
+    redirect_to person_path(@person)
+  end
+
+  private
+    def phone_params
+      params.require(:phone).permit(:number)
+    end
+end
---
+++ app/models/person.rb
@@ -0,0 +1,5 @@
+class Person < ApplicationRecord
+  has_many :phones
+
+  validates :name, presence: true, uniqueness: true, length: { minimum: 5, maximum: 255 }
+end
